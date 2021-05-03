64f8e7d577b781e38efa19035db0e9af99ce9612
Kiran ojjha <kiranojha1226@gmail.com>
2021-05-03 12:18:38 +0545

    Login system completed

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,5 +1,18 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/user-model");
+
+// serialize cookie
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// deserialize cookie
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +23,32 @@ passport.use(
       callbackURL: "/auth/google/redirect",
     },
     (accessToken, refreshToken, profile, done) => {
-      //   passport callbackfunction
-      console.log('passport cb function fired');
       console.log(profile);
+      // check if user already exists
+      User.findOne({ googleId: profile.id })
+        .then((currentUser) => {
+          if (currentUser) {
+            //   already have the user
+            console.log(currentUser + " exists");
+            done(null, currentUser);
+          } else {
+            //   create new user in database
+            new User({
+              username: profile.displayName,
+              googleId: profile.id,
+              thumbnail: profile._json.picture
+            })
+              .save()
+              .then((newUser) => {
+                console.log(newUser + " is saved in database");
+                done(null, newUser);
+              })
+              .catch(() => {
+                console.log("Error occurred saving the user");
+              });
+          }
+        })
+        .catch(console.log("Unexpected error occured"));
     }
   )
 );
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const authRoutes = require("./routes/auth-routes");
+const profileRoutes = require("./routes/profile-routes");
 const passportSetup = require("./config/passport-setup");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 
 // process.env.MONGO_URI
 
@@ -11,17 +14,30 @@ const app = express();
 // set view engine
 app.set("view engine", "ejs");
 
+// set cookie
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [process.env.COOKIE_KEY],
+  })
+);
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 // connect to mongoDB
 mongoose.connect(process.env.MONGO_URI, () => {
-  console.log('Connected to mongoDB');
+  console.log("Connected to mongoDB");
 });
 
 // set up routes
 app.use("/auth", authRoutes);
+app.use("/profile", profileRoutes);
 
 // create home route
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", { user: req.user });
 });
 
 app.listen(5000, () => {
